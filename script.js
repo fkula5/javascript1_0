cb072eca10585db6fe4b8a6be69a202c5a6cc775
Filip Kula <63972969+fkula5@users.noreply.github.com>
2024-10-02 09:31:41 +0000

    feat: 1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 (function () {
-  //TODO
+  const button = document.getElementById("ex1_button");
+  const ex1Cont = document.getElementById("ex1_content");
+  let count = 0;
+
+  button.addEventListener("click", () => {
+    if (count <= 9) {
+      ex1Cont.append(count + ", ");
+      count++;
+    } else {
+      ex1Cont.innerText = "";
+      count = 0;
+    }
+  });
 })();
+
