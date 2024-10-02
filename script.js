7533f41d9a13f156bd4d7de14370914844ad838c
Filip Kula <63972969+fkula5@users.noreply.github.com>
2024-10-02 10:00:11 +0000

    feat:  1_1.2 unfinished

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 (function () {
   const button = document.getElementById("ex1_button");
   const ex1Cont = document.getElementById("ex1_content");
+  const textInput = document.getElementById("ex2_text");
+  const ex2Cont = document.getElementById("ex2_content");
   let count = 0;
+  const format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 
   button.addEventListener("click", () => {
     if (count <= 9) {
@@ -12,5 +15,19 @@
       count = 0;
     }
   });
-})();
 
+  textInput.addEventListener("input", () => {
+    ex2Cont.innerText = "";
+    console.log(textInput.value);
+    let isnum = /^\d+$/.test(textInput.value);
+    if (textInput.value.length != 9) {
+      console.log("Długość numeru musi być równa 9");
+    } else if (!isnum) {
+      console.log("Numer nie może zawierać liter");
+    } else if (!format.test(textInput.value)) {
+      console.log("Numer nie może zawierać znaków specjalnych");
+    } else {
+      console.log("Numer jest poprawny");
+    }
+  });
+})();
