30ad5ac9d7d6964bfc72ac28c250480eb4435201
Filip Kula <63972969+fkula5@users.noreply.github.com>
2024-10-09 07:18:33 +0000

    feat: javascript1_0 done

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
   const ex1Cont = document.getElementById("ex1_content");
   const textInput = document.getElementById("ex2_text");
   const ex2Cont = document.getElementById("ex2_content");
+  const ex3Two = document.getElementById("ex3_two");
+  const ex3One = document.getElementById("ex3_one");
   let count = 0;
   const format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 
@@ -17,17 +19,43 @@
   });
 
   textInput.addEventListener("input", () => {
+    const inputValue = textInput.value;
     ex2Cont.innerText = "";
-    console.log(textInput.value);
-    let isnum = /^\d+$/.test(textInput.value);
-    if (textInput.value.length != 9) {
-      console.log("Długość numeru musi być równa 9");
-    } else if (!isnum) {
-      console.log("Numer nie może zawierać liter");
-    } else if (!format.test(textInput.value)) {
-      console.log("Numer nie może zawierać znaków specjalnych");
+
+    if (inputValue.length !== 9) {
+      ex2Cont.innerText = "Długość numeru musi być równa 9";
+    } else if (/[a-zA-Z]/.test(inputValue)) {
+      ex2Cont.innerText = "Numer nie może zawierać liter";
+    } else if (/[^0-9]/.test(inputValue)) {
+      ex2Cont.innerText = "Numer nie może zawierać znaków specjalnych";
     } else {
-      console.log("Numer jest poprawny");
+      ex2Cont.innerText = "Numer telefonu jest poprawny";
     }
   });
+
+  function dragStartHandler(e) {
+    e.dataTransfer.setData("text/html", e.target.id);
+  }
+
+  window.addEventListener("DOMContentLoaded", () => {
+    const el = document.getElementById("ex3_element");
+    el.addEventListener("dragstart", dragStartHandler);
+  });
+
+  function dragoverHandler(e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+  }
+
+  function dropHandler(e) {
+    e.preventDefault();
+
+    const data = e.dataTransfer.getData("text/html");
+    e.target.appendChild(document.getElementById(data));
+  }
+
+  ex3Two.addEventListener("dragover", dragoverHandler);
+  ex3Two.addEventListener("drop", dropHandler);
+  ex3One.addEventListener("dragover", dragoverHandler);
+  ex3One.addEventListener("drop", dropHandler);
 })();
